8267bd12e354e0831cef2355f63fcd218025c48e
belerico <belo.fede@outlook.com>
2019-04-24 16:07:02 +0200

    Now our beautiful download script shows the download speed! Added folders for storing scripts and results

--- download-matrices.py
+++ download-matrices.py
@@ -2,13 +2,14 @@ import os
 import requests
 import shutil
 import sys
+import time
 
 def download_with_resume(url, file_path):
-    print('Download from ' + url)
-    # don't download if the file exists
+    # Don't download if the file exists
     if os.path.exists(file_path):
         print('File already exists, no need to download')
         return
+    print('Download from ' + url)
     block_size = 1024**2 # 1MB
     tmp_file_path = file_path + '.part'
     first_byte = os.path.getsize(tmp_file_path) if os.path.exists(tmp_file_path) else 0
@@ -18,13 +19,15 @@ def download_with_resume(url, file_path):
     try:
         file_size = int(requests.head(url).headers['Content-length'])
         print('File size is %.1fMB' % (file_size / block_size))
-
+        # Print how many bytes has been already downloaded
+        # Every '=' is 2%. In total there're 50 '='
         done = int((first_byte / file_size) * 50)
         for i in range(done):
             sys.stdout.write('\r[%s%s] %d%%' % ('=' * done, ' ' * (50 - done), done * 2))
             sys.stdout.flush()
-        
+        # Set headers to resume download from where we've left 
         headers = {"Range": "bytes=%s-" % first_byte}
+        start = time.time()
         r = requests.get(url, headers=headers, stream=True)
         with open(tmp_file_path, file_mode) as f:
             for chunk in r.iter_content(chunk_size=block_size):
@@ -32,26 +35,34 @@ def download_with_resume(url, file_path):
                     first_byte += len(chunk)
                     f.write(chunk)
                     done = int(50 * first_byte / file_size)
-                    sys.stdout.write("\r[%s%s] %d%%" % ('=' * done, ' ' * (50 - done), done * 2))
+                    sys.stdout.write("\r[%s%s] %d%% %.1f KB/s" % (
+                        '=' * done, # How many chunk has been downloaded
+                        ' ' * (50 - done), # How many left
+                        done * 2, # Since the total percentage is computed on 50 '=', we multiply by 2 to rescale to 100
+                        (first_byte / 1024) / (time.time() - start)) # v(t) = delta_s / delta_t
+                    )
                     sys.stdout.flush()
             sys.stdout.write('\n')
             sys.stdout.flush()
     except IOError as e:
         print('IO Error - %s' % e)
     finally:
-        # rename the temp download file to the correct name if fully downloaded
+        # Rename the temp download file to the correct name if fully downloaded
         if file_size == os.path.getsize(tmp_file_path):
             shutil.move(tmp_file_path, file_path)
         elif file_size == -1:
             raise Exception('Error getting Content-Length from server: %s' % url)
 
-with open('matrices.txt', 'r') as f:
-    filenames = f.readlines()
-    f.close()
-if not os.path.exists('./matrices'):
-    print('Creating dir for storing matrices')
-    os.makedirs('./matrices')
-for filename in filenames:
-    filename = filename.strip('\n')
-    url = 'https://sparse.tamu.edu/mat/' + filename
-    download_with_resume(url, 'matrices/' + filename.split('/')[1])
+if __name__ == '__main__':
+    if not os.path.exists('matrices.txt'):
+        raise Exception('Missing matrices.txt file')
+    with open('matrices.txt', 'r') as f:
+        filenames = f.readlines()
+        f.close()
+    if not os.path.exists('./matrices'):
+        print('Creating dir for storing matrices')
+        os.makedirs('./matrices')
+    for filename in filenames:
+        filename = filename.strip('\n')
+        url = 'https://sparse.tamu.edu/mat/' + filename
+        download_with_resume(url, 'matrices/' + filename.split('/')[1])
--- scripts/resolution.m
+++ scripts/resolution.m
@@ -6,14 +6,12 @@
 clear
 
 % Test Matrix
-files =  dir('matrices/')
+files =  dir('../matrices/')
 
 for i = 3 : size(files, 1)
-    
     A = load([files(i).folder, '/', files(i).name])
     xe = ones(size(A, 1), 1);
     b = A * xe;
-
     allvars = whos;
     init_memory = sum([allvars.bytes]);
 
@@ -22,21 +20,16 @@ for i = 3 : size(files, 1)
     L = chol(A);
     Y = linsolve(L, b);
     x_sol = linsolve(L, Y);
-
     toc
 
     %% Check
-
     allvars = whos;
     ending_memory = sum([allvars.bytes]);
-
     if (uint8(x_sol) == xe)
         display([ "La matrice: ", files(i).name])
         display("Ok la soluzione � corretta");
         e = norm(x_sol - xe) / norm(xe);
         display(sprintf("L'errore � %d", e));
         display(sprintf("La memoria usata (in Byte) � %d", (ending_memory - init_memory)));
-
-
     end
 end
