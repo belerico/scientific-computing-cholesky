28b021727b9caeccb4054ad9eace90fe14d82bfa
Gianscarpe <gianlucascarpellini@yahoo.it>
2019-04-26 12:48:18 +0000

    .

--- scripts/resolution.m
+++ scripts/resolution.m
@@ -9,28 +9,28 @@ files =  dir('../matrices/');
 
 for i = 3 : size(files, 1)
     M = load([files(i).folder, '/', files(i).name]);
-    A = M.Problem.A;
-    xe = ones(size(A, 1), 1);
+    xe = ones(size(M.Problem.A, 1), 1);
+    x_sol = []
     disp('Compute b')
-    b = A * xe;
-    allvars = whos;
-    init_memory = sum([allvars.bytes]);
+    b = M.Problem.A * xe;
 
     %% Solve the system
     disp('Solving system with Cholesky factorization...')
+    allvars = whos;
+    init_memory = sum([allvars.bytes]);
     tic
-    x_sol = A \ b;
+    x_sol = M.Problem.A \ b;
     toc
+    allvars = whos;
+    ending_memory = sum([allvars.bytes]);
     disp('Done')
     
     %% Check
-    allvars = whos;
-    ending_memory = sum([allvars.bytes]);
     if (uint8(x_sol) == xe)
         disp([ "La matrice: ", files(i).name])
         disp("Ok la soluzione e' corretta");
         e = norm(x_sol - xe) / norm(xe);
-        fprintf("L'errore e' %d", e);
-        fprintf("La memoria usata (in Byte) e' %d", (ending_memory - init_memory));
+        disp(fprintf("L'errore e' %d", e));
+        disp(fprintf("La memoria usata (in Byte) e' %d", (ending_memory - init_memory)));
     end
 end
