00bfe6e1b8b1f4516d1ed8d6a177e7fbd7f263d3
belerico <belo.fede@outlook.com>
2019-04-26 12:48:38 +0200

    Added log info to resolution.m

--- scripts/resolution.m
+++ scripts/resolution.m
@@ -2,34 +2,40 @@
 % per lanciare questo script bisogna creare una cartella 'matrices' e
 % metterci dentro le matrici da analizzare
 %%%
-
 clear
 
 % Test Matrix
-files =  dir('../matrices/')
+files =  dir('matrices/');
 
 for i = 3 : size(files, 1)
-    A = load([files(i).folder, '/', files(i).name])
+    disp(['Loading' ' ' files(i).name])
+    matFile = load([files(i).folder, '/', files(i).name]);
+    disp('Setting matrix A')
+    A = matFile.Problem.A;
+    disp('Compute exact solution xe')
     xe = ones(size(A, 1), 1);
+    disp('Compute b')
     b = A * xe;
     allvars = whos;
     init_memory = sum([allvars.bytes]);
 
     %% Solve the system
+    disp('Solving system with Cholesky factorization...')
     tic
     L = chol(A);
     Y = linsolve(L, b);
     x_sol = linsolve(L, Y);
     toc
-
+    disp('Done')
+    
     %% Check
     allvars = whos;
     ending_memory = sum([allvars.bytes]);
     if (uint8(x_sol) == xe)
-        display([ "La matrice: ", files(i).name])
-        display("Ok la soluzione � corretta");
+        disp([ "La matrice: ", files(i).name])
+        disp("Ok la soluzione e' corretta");
         e = norm(x_sol - xe) / norm(xe);
-        display(sprintf("L'errore � %d", e));
-        display(sprintf("La memoria usata (in Byte) � %d", (ending_memory - init_memory)));
+        fprintf("L'errore e' %d", e);
+        fprintf("La memoria usata (in Byte) e' %d", (ending_memory - init_memory));
     end
 end
