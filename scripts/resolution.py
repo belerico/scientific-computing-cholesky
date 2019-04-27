630d34bd6ea2f6120f03fe426b6c1b2ae3fa9991
belerico <belo.fede@outlook.com>
2019-04-27 18:30:37 +0200

    Added elapsed time for the python script

--- scripts/resolution.py
+++ scripts/resolution.py
@@ -1,4 +1,5 @@
 import os
+import time
 import numpy
 from scipy import io
 from scipy import linalg
@@ -10,20 +11,23 @@ RESULTS_DIR = os.path.join(CWD, 'results')
 
 matrices = os.listdir(MATRICES_DIR)
 
-for matrix in matrices:
+for matrix in ['shallow_water1.mat']:
     A = io.loadmat(os.path.join(MATRICES_DIR, matrix))['Problem']['A'][0][0]
     print('Resolving for ' + matrix)
-    xe = numpy.ones([ A.shape[0], 1])
+    xe = numpy.ones([A.shape[0], 1])
     b = A * xe 
     """ L = linalg.cholesky(A, lower=True)
     y = linalg.solve(L, b)
     x = linalg.solve(numpy.transpose(L), y) """
     print('Decomposing matrix with Cholesky decomposition')
+    start = time.time()
     factor = cholmod.cholesky(A)
     print('Done')
     print('Resolving equation Ax=b')
     x = factor(b)
+    end = time.time()
     print('Done')
     if numpy.allclose(x, xe):
         e = linalg.norm(x -xe) / linalg.norm(xe)
         print('Error: ', e)
+        print('Elapsed time:', str(end - start))
