acbef8b8f67ca4739416512a70b7f7fdd4387f86
belerico <belo.fede@outlook.com>
2019-04-29 22:37:39 +0200

    resolution.py now print time stats to resolution_os-name_result.txt. Scarpe REMEMBER TO PUT RESULT FILES TO results DIR :)

--- scripts/resolution.py
+++ scripts/resolution.py
@@ -4,30 +4,34 @@ import numpy
 from scipy import io
 from scipy import linalg
 from sksparse import cholmod
+import platform
 
 CWD = os.getcwd()
 MATRICES_DIR = os.path.join(CWD, 'matrices')
 RESULTS_DIR = os.path.join(CWD, 'results')
-
 matrices = os.listdir(MATRICES_DIR)
 
-for matrix in matrices:
-    A = io.loadmat(os.path.join(MATRICES_DIR, matrix))['Problem']['A'][0][0]
-    print('Resolving for ' + matrix)
-    xe = numpy.ones([A.shape[0], 1])
-    b = A * xe 
-    """ L = linalg.cholesky(A, lower=True)
-    y = linalg.solve(L, b)
-    x = linalg.solve(numpy.transpose(L), y) """
-    print('Decomposing matrix with Cholesky decomposition')
-    start = time.time()
-    factor = cholmod.cholesky(A)
-    print('Done')
-    print('Resolving equation Ax=b')
-    x = factor(b)
-    end = time.time()
-    print('Done')
-    if numpy.allclose(x, xe):
-        e = linalg.norm(x -xe) / linalg.norm(xe)
-        print('Error: ', e)
-        print('Elapsed time:', str(end - start))
+def resolution(matrix):
+        A = io.loadmat(os.path.join(MATRICES_DIR, matrix))['Problem']['A'][0][0]
+        xe = numpy.ones([A.shape[0], 1])
+        b = A * xe 
+        start = time.time()
+        factor = cholmod.cholesky(A)
+        x = factor(b)
+        end = time.time()
+        elapsed = end - start
+        return xe, x, elapsed 
+
+if __name__ == '__main__':
+        for matrix in matrices:
+                results = 'Resolving for: ' + matrix + '\n'
+                xe, x, elapsed = resolution(matrix)
+                if numpy.allclose(x, xe):
+                        e = linalg.norm(x -xe) / linalg.norm(xe)
+                        results += 'Error: ' + str(e) + '\n'
+                        results += 'Elapsed time: ' + str(elapsed) + '\n\n'
+                else:
+                        results += 'Solution is not even close to exact solution' + '\n\n'
+                f = open(os.path.join(RESULTS_DIR, 'python_' + str.lower(platform.system()) + '_result.txt'), 'a')
+                f.write(results)
+                f.close()
