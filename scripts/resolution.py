06225513c6228e97e6cba3e96bac27ff302a2de5
belerico <belo.fede@outlook.com>
2019-04-29 23:25:52 +0200

    Added memory cosumption to resolution.py

--- scripts/resolution.py
+++ scripts/resolution.py
@@ -1,10 +1,11 @@
 import os
 import time
 import numpy
+import platform
+import tracemalloc
 from scipy import io
 from scipy import linalg
 from sksparse import cholmod
-import platform
 
 CWD = os.getcwd()
 MATRICES_DIR = os.path.join(CWD, 'matrices')
@@ -12,6 +13,7 @@ RESULTS_DIR = os.path.join(CWD, 'results')
 matrices = os.listdir(MATRICES_DIR)
 
 def resolution(matrix):
+        tracemalloc.start()
         A = io.loadmat(os.path.join(MATRICES_DIR, matrix))['Problem']['A'][0][0]
         xe = numpy.ones([A.shape[0], 1])
         b = A * xe 
@@ -19,16 +21,21 @@ def resolution(matrix):
         factor = cholmod.cholesky(A)
         x = factor(b)
         end = time.time()
+        snapshot = tracemalloc.take_snapshot()
+        statistics = snapshot.statistics('lineno')
         elapsed = end - start
-        return xe, x, elapsed 
+        # Occupied memory in bytes
+        mem = sum(stat.size for stat in statistics)
+        return xe, x, elapsed, mem
 
 if __name__ == '__main__':
         for matrix in matrices:
                 results = 'Resolving for: ' + matrix + '\n'
-                xe, x, elapsed = resolution(matrix)
+                xe, x, elapsed, mem = resolution(matrix)
                 if numpy.allclose(x, xe):
                         e = linalg.norm(x -xe) / linalg.norm(xe)
                         results += 'Error: ' + str(e) + '\n'
+                        results += 'Memory occupied: ' + str(mem) + ' B, ' + str(mem / 1024) + ' KiB, ' + str(mem / (1024 ** 2)) + ' MiB\n'
                         results += 'Elapsed time: ' + str(elapsed) + '\n\n'
                 else:
                         results += 'Solution is not even close to exact solution' + '\n\n'
