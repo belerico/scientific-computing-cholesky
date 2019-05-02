2486a01012494e014feda3a94be3520575b5800d
belerico <belo.fede@outlook.com>
2019-05-02 10:53:07 +0200

    Change memory profiler from psrecord to mprof due to issues with Widows OS

--- scripts/get-statistics.py
+++ scripts/get-statistics.py
@@ -9,16 +9,20 @@ OS = platform.system().lower()
 matrices = os.listdir(MATRICES_DIR)
 
 if __name__ == '__main__':
-    for tool in ['python', 'matlab']:
+    for tool in ['python']:
         if not(os.path.exists(os.path.join(RESULTS_DIR, tool, OS))):
                 os.makedirs(os.path.join(RESULTS_DIR, tool, OS))
         for matrix in matrices:
-            command = ''
+            command = 'mprof run --include-children --interval 0.05 --output ' + os.path.join(RESULTS_DIR, tool, OS, matrix.split('.')[0] + '.txt') + ' '
             if tool == 'matlab':
-                command = 'psrecord "matlab -nodisplay -nosplash -nodesktop -r \\"addpath(genpath(pwd));resolution(\'' + matrix + '\');exit;\\"" '
+                # command = 'psrecord "matlab -nodisplay -nosplash -nodesktop -r \\"addpath(genpath(pwd));resolution(\'' + matrix + '\');exit;\\"" '
+                command += 'matlab -nodisplay -nosplash -nodesktop -r "addpath(genpath(pwd));resolution(\'' + matrix + '\');exit;"'
             elif tool == 'python':
-                command = 'psrecord "python3 ' + os.path.join(CWD, 'resolution.py') + ' ' + matrix + '" '
-            command += '--include-children \
+                # command = 'psrecord "python3 ' + os.path.join(CWD, 'resolution.py') + ' ' + matrix + '" '
+                command += 'scripts/resolution.py \
+                            ' + os.path.join(MATRICES_DIR, matrix) + ' \
+                            ' + RESULTS_DIR
+            """ command += '--include-children \
                         --interval 0.05 \
-                        --log ' + os.path.join(RESULTS_DIR, tool, OS, matrix.split('.')[0] + '.txt')
+                        --log ' + os.path.join(RESULTS_DIR, tool, OS, matrix.split('.')[0] + '.txt') """
             proc = subprocess.call(command, shell=True)
--- scripts/resolution.py
+++ scripts/resolution.py
@@ -3,37 +3,46 @@ import sys
 import time
 import numpy
 import platform
+import tracemalloc
 from scipy import io
 from scipy import linalg
 from sksparse import cholmod
 
-CWD = os.path.dirname(os.path.abspath(__file__))
-MATRICES_DIR = os.path.join(CWD, '..', 'matrices')
-RESULTS_DIR = os.path.join(CWD, '..', 'results')
-matrices = os.listdir(MATRICES_DIR)
+# CWD = os.path.dirname(os.path.abspath(__file__))
+# MATRICES_DIR = os.path.join(CWD, '..', 'matrices')
+# RESULTS_DIR = os.path.join(CWD, '..', 'results')
+# matrices = os.listdir(MATRICES_DIR)
 
 def resolve(matrix):
-        A = io.loadmat(os.path.join(MATRICES_DIR, matrix))['Problem']['A'][0][0]
+        tracemalloc.start()
+        A = io.loadmat(matrix)['Problem']['A'][0][0]
         xe = numpy.ones([A.shape[0], 1])
         b = A * xe 
         start = time.time()
         factor = cholmod.cholesky(A)
         x = factor(b)
         end = time.time()
+        snapshot = tracemalloc.take_snapshot()
+        stats = snapshot.statistics('lineno')
+        mem = sum(stat.size for stat in stats)
         elapsed = end - start
-        return xe, x, elapsed
+        return xe, x, elapsed, mem
 
 if __name__ == '__main__':
-        # for matrix in matrices:
+        """
+                :param str sys.argv[1]: Matrix path to be loaded
+                :param str sys.argv[2]: Results dir to write results per matrix
+        """
         matrix = sys.argv[1]
         results = 'Resolving ' + matrix + '\n'
-        xe, x, elapsed = resolve(matrix)
+        xe, x, elapsed, mem = resolve(matrix)
         if numpy.allclose(x, xe):
                 e = linalg.norm(x -xe) / linalg.norm(xe)
                 results += 'Error: ' + str(e) + '\n'
-                results += 'Elapsed time: ' + str(elapsed) + ' s\n\n'
+                results += 'Elapsed time: ' + str(elapsed) + ' s\n'
+                results += 'Occupied memory: %.2f MiB\n\n' % (mem / (1024 ** 2))
         else:
                 results += 'Solution is not even close to exact solution' + '\n\n'
-        f = open(os.path.join(RESULTS_DIR, 'python_' + str.lower(platform.system()) + '_result.txt'), 'a')
+        f = open(os.path.join(sys.argv[2], 'python_' + str.lower(platform.system()) + '_result.txt'), 'a')
         f.write(results)
         f.close()
